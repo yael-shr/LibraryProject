2a67e30c890c2d8766d03ebb4af5975004630b56
yael shraga <yael216220@gmail.com>
2026-03-05 16:56:30 +0200

    add update and add logic to book services and components

--- client/src/app/components/book-edit/book-edit.ts
+++ client/src/app/components/book-edit/book-edit.ts
@@ -1,9 +1,44 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { BookService } from '../../services/book';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-book-edit',
-  imports: [],
-  templateUrl: './book-edit.html',
-  styleUrl: './book-edit.css',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './book-edit.html'
 })
-export class BookEdit {}
+export class BookEdit implements OnInit {
+  bookForm: FormGroup;
+  isEditMode = false;
+
+  constructor(private fb: FormBuilder, private bookService: BookService) {
+    this.bookForm = this.fb.group({
+      Id: [null],
+      Title: ['', Validators.required],
+      AuthorName: ['', Validators.required],
+      Description: [''],
+      CategoryName: [''],
+      StatusName: ['']
+    });
+  }
+
+  ngOnInit(): void {}
+
+  save() {
+    if (this.bookForm.valid) {
+      if (this.isEditMode) {
+        // הקומפוננטה לא יודעת מה השם של הפרוצדורה, היא רק קוראת לפונקציה
+        this.bookService.updateBook(this.bookForm.value).subscribe(() => {
+          alert('עודכן בהצלחה!');
+        });
+      } else {
+        this.bookService.addBook(this.bookForm.value).subscribe(() => {
+          alert('נוסף בהצלחה!');
+        });
+      }
+    }
+  }
+}
--- client/src/app/services/book.ts
+++ client/src/app/services/book.ts
@@ -22,6 +22,10 @@ export class BookService {
     return this.http.post<any>(this.apiUrl, request);
   }
 
+  public executeSP(spName: string, parameters: any = {}): Observable<any> {
+    return this.execute(spName, parameters);
+  }
+
   getAllBooks(): Observable<Book[]> {
     return this.execute('Books_GetAll_Search');
   }
@@ -30,7 +34,14 @@ export class BookService {
     return this.execute('DeleteBook', { id: bookId });
   }
 
-  // במקום מה שיש כרגע, תנסי את זה:
+  addBook(book: Book): Observable<any> {
+    return this.execute('Books_Add',  book);
+  }
+
+  updateBook(book: Book): Observable<any> {
+    return this.execute('Books_Update', book);
+  }
+
 getBookById(bookId: number): Observable<Book> {
   return this.execute('Books_GetById', { id: bookId });
 }
